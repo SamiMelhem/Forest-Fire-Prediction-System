232905be84add3503e844fd9dcf3f58696ec1a5b
Kevin Zhang <kevzhang2022@gmail.com>
2025-01-26 04:56:20 -0600

    WIP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
         Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
         Zoom: {zoom.toFixed(2)}
       </div>
-      {/* <div className="w-[440px] min-h-[600px] absolute top-[55px] left-[12px] z-10 bg-white rounded-md p-2 shadow-lg">
+      <div className="w-[440px] min-h-[600px] absolute top-[55px] left-[12px] z-10 bg-white/80 rounded-md p-2 shadow-lg bg-opac">
         Hello World!
-      </div> */}
+      </div>
       <div className="flex gap-2 absolute top-[670px] left-[12px] z-10">
         <button
           className="bg-white rounded-md px-4 py-1 shadow-lg hover:cursor-pointer"
